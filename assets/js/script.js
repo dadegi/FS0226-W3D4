56db17b8afde93da77c17976e148f5b7cbe15ed8
Naviganet <dadegi@gmail.com>
2026-05-21 10:55:24 +0200

    Codici lezione U1-W3-D4

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,3 +35,9 @@ const printCurrentMonth = () => {
 };
 
 printCurrentMonth();
+
+const dayInMonth = () => {
+	const lastDay = new Date(getYear, getMonth + 1, 0); // Glistiamo chiedendo il giorno 0 del mese successivo, ad esempio, lo zero giugno 2026, che sarà il 31 maggio 2026
+	const numberOfDays = lastDay.getDate();
+	return numberOfDays;
+};
